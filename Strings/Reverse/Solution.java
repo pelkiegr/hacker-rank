c4ff75e1eb2dfe27e1a874d6bca994920514026e
Greg Pelkie <pelkiegr@gmail.com>
2019-04-09 17:15:13 -0600

    Finish strings

--- Strings/Reverse/Solution.java
+++ Strings/Reverse/Solution.java
@@ -7,7 +7,20 @@ public class Solution {
         
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
         
+        System.out.println(isPalindrome(A) ? "Yes" : "No");
+    }
+
+    private static boolean isPalindrome(String str) {
+        char[] arr = str.toCharArray();
+
+        int length = str.length();
+        for (int i = 0; i < length/2; i ++) {
+            if (arr[i] != arr[length - i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
